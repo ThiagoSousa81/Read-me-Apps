3abdb433e1e9d33f9f84a1d930e55cd3aa0ebb04
Thiago Sousa <thiagosousa81@gmail.com>
2023-12-02 21:03:11 -0300

    §

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+'''from flask import Flask
 
 app = Flask(__name__)
 
@@ -9,3 +9,20 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+    '''
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/fortunes', methods=['GET'])
+def get_fortunes():
+    fortunes = [
+        {'id': 1, 'message': 'You will soon find yourself in a favorable position.'},
+        {'id': 2, 'message': 'A new opportunity is coming your way.'},
+        {'id': 3, 'message': 'Good luck will soon shine upon you.'}
+    ]
+    return jsonify(fortunes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
