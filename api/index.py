bd51d182d9b573b8cc29f226aa88433d3adbd8b1
Thiago Sousa <thiagosousa81@gmail.com>
2023-12-02 21:08:35 -0300

    §

--- api/index.py
+++ api/index.py
@@ -10,19 +10,26 @@ def home():
 def about():
     return 'About'
     '''
-
 from flask import Flask, request, jsonify
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/fortunes', methods=['GET'])
-def get_fortunes():
-    fortunes = [
-        {'id': 1, 'message': 'You will soon find yourself in a favorable position.'},
-        {'id': 2, 'message': 'A new opportunity is coming your way.'},
-        {'id': 3, 'message': 'Good luck will soon shine upon you.'}
-    ]
-    return jsonify(fortunes)
+@app.route('/', methods=['GET'])
+def index():
+    return 'Welcome to my API!'
+
+@app.route('/equations', methods=['GET'])
+def equations():
+    equation = request.args.get('equation')
+    if equation == None:
+        return 'No equation provided', 400
+    try:
+        num1, num2, var = map(float, equation.split())
+        result = np.solve([num1, num2], var)
+        return jsonify({'result': result})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
