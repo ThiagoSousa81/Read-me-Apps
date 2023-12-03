4592ec0187152c0f7ad5ba7f0e9d92e95ff643a3
Thiago Sousa <thiagosousa81@gmail.com>
2023-12-02 21:17:00 -0300

    §

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-'''from flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -9,27 +9,3 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
-    '''
-from flask import Flask, request, jsonify
-import numpy as np
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def index():
-    return 'Welcome to my API!'
-
-@app.route('/equations', methods=['GET'])
-def equations():
-    equation = request.args.get('equation')
-    if equation == None:
-        return 'No equation provided', 400
-    try:
-        num1, num2, var = map(float, equation.split())
-        result = np.solve([num1, num2], var)
-        return jsonify({'result': result})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
-
-if __name__ == '__main__':
-    app.run(debug=True)
